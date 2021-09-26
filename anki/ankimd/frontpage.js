71dd72a12dd8edd42b029a572c3d8dd21c6f3217
Klaus <no.mail.to.grab@here.com>
2021-09-26 18:23:32 +0200

    quick commit

--- anki/ankimd/frontpage.js
+++ anki/ankimd/frontpage.js
@@ -6,6 +6,11 @@ function renderQuestion() {
     } else {
         document.getElementById("display").innerHTML = document.getElementById("id").innerHTML;
     }
+
+    var notes = document.getElementById("notes").innerHTML.trim();
+    if (notes.indexOf("#") !== -1) {
+        document.getElementById("display").innerHTML += "<br><br><div style='font-size: 0.8em'>" + notes + "</div>" + "\n\n"; // "<div id='notes-addition'>"+notes+"</div>"
+    }
 }
 
 renderQuestion();
